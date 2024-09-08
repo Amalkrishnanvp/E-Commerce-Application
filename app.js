2754b50de013253ac1168a75ad22d3df88526d1b
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-09-08 23:13:50 +0530

    Added custom layout

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const hbs = require("hbs");
-
+const path = require("path");
 const app = express();
 const port = 5000;
+const hbs = require("hbs");
+
+// Set view engine to hbs
+app.set("view engine", "hbs");
+
+// Set the views directory
+app.set("views", path.join(__dirname, "views"));
+
+// Register the layout directory
+app.set("view options", { layout: "layouts/layout" });
+
+// Register the partials directory
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 // Import routes
 const indexRouter = require("./routes/index");
