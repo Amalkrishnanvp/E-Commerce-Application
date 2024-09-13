fc7e29541ca0a9673eecf9d638e23ee8c5f9859f
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-09-13 12:07:06 +0530

    Added products table in admin page

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,34 @@ const router = express.Router();
 
 /* GET Admin page */
 router.get("/", (req, res, next) => {
-  res.render("index", { admin: true });
+  let products = [
+    {
+      name: "Earthen Bottle",
+      category: "kitchen",
+      price: "45",
+      image: "/images/img1.jpg",
+    },
+    {
+      name: "Nomad Tumbler",
+      category: "kitchen",
+      price: "48",
+      image: "/images/img2.jpg",
+    },
+    {
+      name: "Paper Refill",
+      category: "kitchen",
+      price: "50",
+      image: "/images/img3.jpg",
+    },
+    {
+      name: "Mechanical Pencil",
+      category: "kitchen",
+      price: "22",
+      image: "/images/img4.jpg",
+    },
+  ];
+
+  res.render("admin/view-products", { admin: true, products });
 });
 
 module.exports = router;
