26076eadc35c1112ea93b8579c75a1bcd74b24ee
Amalkrishnanvp <Amalyedhu10@gmail.com>
2025-05-18 15:47:59 +0530

    added process.env.port in backend code

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 5000;
+const port = process.env.port || 5000;
 const hbs = require("hbs");
 const fileUpload = require("express-fileupload");
 const { connectToDb } = require("./config/connection");
